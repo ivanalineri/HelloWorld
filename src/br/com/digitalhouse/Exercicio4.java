7104b4afc048e7593d22ea69a589595a93bf17d4
Ivan C. Alineri <ivan.alineri@outlook.com>
2019-04-04 16:09:22 -0300

    alteracao 4

--- src/br/com/digitalhouse/Exercicio4.java
+++ src/br/com/digitalhouse/Exercicio4.java
@@ -5,12 +5,15 @@ public class Exercicio4 {
     public static void main(String[] args) {
         int[] soma;
         soma = new int[4];
-        soma[0] = 100;
-        soma[1] = 84;
-        soma[2] = 33;
-        soma[3] = 109;
+        soma[0] = 1;
+        soma[1] = 2;
+        soma[2] = 4;
+        soma[3] = 5;
 
-        System.out.println((soma[0])+(soma[1])+(soma[2])+soma[3]);
-
+       //System.out.println((soma[0])+(soma[1])+(soma[2])+soma[3]);
+        for( int i = 0; i < soma.length; i++){
+            soma = soma + soma[i]
+        }
+        System.out.println(soma);
     }
 }
